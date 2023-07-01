db12aacf5bf6c2f626432509938a77a5ecc5b2a0
Tu Nombre <you@example.com>
2023-07-01 09:10:12 -0400

    halloween website green theme  terminado

---
+++ README.md
@@ -0,0 +1,8 @@
+# Halloween web
+
+Halloween web elaborada con HTML & CSS con diseño responsive
+
+---
+
+Code by gavdev1
+Design by Bedimcode: https://youtu.be/lgo1CEPZoxg
---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const bar = document.querySelector("#bar");
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("#nav");
+const close = document.querySelector("#close");
+
+/*=============== SHOW MENU ===============*/
+
+bar.addEventListener("click", () => menu.classList.add("show-menu"));
+
+close.addEventListener("click", () => menu.classList.remove("show-menu"));
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+document.addEventListener("scroll", () => window.scrollY > 50 ? nav.classList.add("bg-visible") : nav.classList.remove("bg-visible"));
+
